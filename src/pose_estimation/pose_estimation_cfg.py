c12d6e4b1f56b89c2cd1f2aaf14e639af7a56c0f
Abhijoy-Mandal <a.mandal@mail.utoronto.ca>
2024-07-12 14:39:07 -0400

    now tracking multiple objects

--- src/pose_estimation/pose_estimation_cfg.py
+++ src/pose_estimation/pose_estimation_cfg.py
@@ -1,4 +1,4 @@
 import os
 code_dir = os.path.dirname(os.path.realpath(__file__))
-meshes = ['cube.obj','beaker_250ml.obj']
-meshes = [f'{code_dir}/perception_data/objects/{mesh}' for mesh in meshes]
+mesh_files = ['cube.obj','beaker_250ml.obj']
+meshes = [f'{code_dir}/perception_data/objects/{mesh}' for mesh in mesh_files]
