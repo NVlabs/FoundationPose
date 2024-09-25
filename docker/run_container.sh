a408ea8a76895519608c5600d4de37a3e647764a
Amelie Kammerer <amelie.kammerer@bmw.de>
2024-09-25 15:27:46 +0200

    feat: setting up working docker compose
    still the need for a post install after starting the container

--- docker/run_container.sh
+++
@@ -1,3 +0,0 @@
-docker rm -f foundationpose
-CATGRASP_DIR=$(pwd)/../
-xhost +  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v /home/q502262/foundationpose:/home/q502262/foundationpose -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE foundationpose:latest bash
